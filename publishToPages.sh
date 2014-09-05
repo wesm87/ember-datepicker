dc88903c2c70633254f31417df1c744ae38f6fb6
Nico Gevers <nico@squire-labs.com>
2014-09-05 09:35:49 +0200

    Updated publishToPages

--- publishToPages.sh
+++ publishToPages.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 git branch -D gh-pages
+git push origin --delete gh-pages
 git checkout -b gh-pages
 ember build --environment production
-git rm -rf app app-addon config lib public tests vendor-addon Brocfile bower.json package.json testem.json
+git rm -rf app app-addon config lib public tests vendor-addon
+git rm -rf Brocfile.js bower.json package.json testem.json
 mv dist/* .
 rm -rf dist
 git add .
 git commit -m "Publishing to github pages"
+git push origin gh-pages
+git checkout master
