4113ee4549fbb96d9bb96782d06f041997902aa7
Nico Gevers <nico@squire-labs.com>
2014-10-09 09:51:09 +0200

    Changed momentjs to moment

--- Brocfile.js
+++ Brocfile.js
@@ -17,8 +17,8 @@ var app = new EmberAddon();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('bower_components/momentjs/moment.js');
-app.import('bower_components/pikaday/pikaday.js');
-app.import('bower_components/pikaday/css/pikaday.css');
+app.import(app.bowerDirectory + '/moment/moment.js');
+app.import(app.bowerDirectory + '/pikaday/pikaday.js');
+app.import(app.bowerDirectory + '/pikaday/css/pikaday.css');
 
 module.exports = app.toTree();
